717f9b441a5259034a0ece959bbee5be1d4379f5
Ruslan Svirschevski <ruslansv@gmail.com>
2024-04-11 12:01:56 +0300

    extra tests 4d mask

--- tests/models/llama/test_modeling_llama.py
+++ tests/models/llama/test_modeling_llama.py
@@ -829,7 +829,7 @@ class Mask4DTestHard(unittest.TestCase):
 
         # regular batch
         logits = self.model.forward(input_ids, position_ids=position_ids).logits
-        logits_last = logits[:, -1, :]  # last tokens in each batch line
+        logits_last = logits[:, -1, :]  # last tokens in each batch line    
         decoded = [self.tokenizer.decode(t) for t in logits_last.argmax(dim=-1)]
 
         # 2 forward runs with custom 4D masks
@@ -863,6 +863,131 @@ class Mask4DTestHard(unittest.TestCase):
         mask_1c = mask_shared_prefix
         outs_1c = self.model.forward(
             input_1c, attention_mask=mask_1c.bool(), position_ids=position_ids_1c, past_key_values=past_key_values_a
+        )
+        decoded_1c = [
+            self.tokenizer.decode(t)
+            for t in outs_1c.logits.argmax(-1)[
+                0, torch.where(position_ids_shared_prefix == position_ids_shared_prefix.max())[1] - part_a
+            ]
+        ]
+        self.assertEqual(decoded, decoded_1c)
+
+    def test_stacked_causal_mask_static_cache(self):
+        """same as above but with StaticCache"""
+        (
+            input_ids,
+            position_ids,
+            input_ids_shared_prefix,
+            mask_shared_prefix,
+            position_ids_shared_prefix,
+        ) = self.get_test_data()
+
+        # regular batch
+        logits = self.model.forward(input_ids, position_ids=position_ids).logits
+        logits_last = logits[:, -1, :]  # last tokens in each batch line
+        decoded = [self.tokenizer.decode(t) for t in logits_last.argmax(dim=-1)]
+
+        # upgrade the model with StaticCache
+        max_cache_len = 16  # note that max_cache_len is greater than the attention_mask.shape[-1]
+        self.model._setup_cache(StaticCache, 1, max_cache_len=max_cache_len)
+
+        padded_attention_mask = torch.nn.functional.pad(input=mask_shared_prefix.bool(), 
+                                           pad=(0, max_cache_len - mask_shared_prefix.shape[-1]), 
+                                           mode="constant", 
+                                           value=False)
+
+        # single forward run with 4D custom mask
+        logits_shared_prefix = self.model.forward(
+            input_ids_shared_prefix,
+            attention_mask=padded_attention_mask,
+            position_ids=position_ids_shared_prefix,
+            cache_position=torch.arange(input_ids_shared_prefix.shape[-1], device=torch_device),
+        ).logits
+        logits_shared_prefix_last = logits_shared_prefix[
+            0, torch.where(position_ids_shared_prefix == position_ids_shared_prefix.max())[1], :
+        ]  # last three tokens
+        decoded_shared_prefix = [self.tokenizer.decode(t) for t in logits_shared_prefix_last.argmax(dim=-1)]
+
+        self.assertEqual(decoded, decoded_shared_prefix)
+
+    def test_partial_stacked_causal_mask_static_cache(self):
+        # Same as the test above, but the input is passed in two groups. It tests that we can pass partial 4D attention
+        # masks
+
+        (
+            input_ids,
+            position_ids,
+            input_ids_shared_prefix,
+            mask_shared_prefix,
+            position_ids_shared_prefix,
+        ) = self.get_test_data()
+
+        # regular batch
+        logits = self.model.forward(input_ids, position_ids=position_ids).logits
+        logits_last = logits[:, -1, :]  # last tokens in each batch line
+        decoded = [self.tokenizer.decode(t) for t in logits_last.argmax(dim=-1)]
+
+        # upgrade the model with StaticCache
+        max_cache_len = 16  # note that max_cache_len is greater than the attention_mask.shape[-1]
+        self.model._setup_cache(StaticCache, 1, max_cache_len=max_cache_len)
+
+        # 2 forward runs with custom 4D masks
+        part_a = 3  # split point
+
+        input_1a = input_ids_shared_prefix[:, :part_a]
+        position_ids_1a = position_ids_shared_prefix[:, :part_a]
+        mask_1a = mask_shared_prefix[:, :, :part_a, :part_a]
+
+        padded_mask_1a = torch.nn.functional.pad(input=mask_1a,
+                                                 pad=(0, max_cache_len - mask_1a.shape[-1]),
+                                                 mode="constant",
+                                                 value=0)
+
+        outs_1a = self.model.forward(input_1a,
+                                     attention_mask=padded_mask_1a.bool(),
+                                     position_ids=position_ids_1a,
+                                     cache_position=torch.arange(part_a, device=torch_device),
+                                     )
+
+        # Case 1: we pass a 4D attention mask regarding the current sequence length (i.e. [..., seq_len, full_len])
+        input_1b = input_ids_shared_prefix[:, part_a:]
+        position_ids_1b = position_ids_shared_prefix[:, part_a:]
+        mask_1b = mask_shared_prefix[:, :, part_a:, :]
+        
+        padded_mask_1b = torch.nn.functional.pad(input=mask_1b,
+                                            pad=(0, max_cache_len - mask_1b.shape[-1]),
+                                            mode="constant",
+                                            value=0)
+
+        outs_1b = self.model.forward(
+            input_1b, 
+            attention_mask=padded_mask_1b.bool(), 
+            position_ids=position_ids_1b, 
+            cache_position=torch.arange(part_a, input_ids_shared_prefix.shape[-1], device=torch_device),
+        )
+        decoded_1b = [
+            self.tokenizer.decode(t)
+            for t in outs_1b.logits.argmax(-1)[
+                0, torch.where(position_ids_shared_prefix == position_ids_shared_prefix.max())[1] - part_a
+            ]
+        ]
+        self.assertEqual(decoded, decoded_1b)
+
+        # Case 2: we pass a 4D attention mask regarding the full sequence length (i.e. [..., full_len, full_len])
+        input_1c = input_ids_shared_prefix[:, part_a:]
+        position_ids_1c = position_ids_shared_prefix[:, part_a:]
+
+        mask_1c = mask_shared_prefix
+        padded_mask_1c = torch.nn.functional.pad(input=mask_1c,
+                                    pad=(0, max_cache_len - mask_1c.shape[-1]),
+                                    mode="constant",
+                                    value=0)
+
+        outs_1c = self.model.forward(
+            input_1c, 
+            attention_mask=padded_mask_1c.bool(), 
+            position_ids=position_ids_1c, 
+            cache_position=torch.arange(part_a, input_ids_shared_prefix.shape[-1], device=torch_device),
         )
         decoded_1c = [
             self.tokenizer.decode(t)
